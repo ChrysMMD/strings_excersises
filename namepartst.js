54db8a2f5ecb6b55598d17e265e4ec2369ca43ae
Chrys <chjo0001@stud.kea.dk>
2024-02-09 11:32:52 +0100

    nameparts

---
+++ namepartst.js
@@ -0,0 +1,32 @@
+// const name = "Peter";
+// let animal = "cat";
+// let animalName = "Mandu";
+
+// console.log(`My name is ${name}.\nI have a ${animal} called ${animalName}.`)
+
+// const name = "Peter";
+// const len = name.length;
+// const letter = name[0];
+
+// console.log(`${name} is ${len} charachters long`)
+// console.log(`The first letter of ${name} is ${letter}`);
+
+// const name = "Albus Percival Wulfric Brian Dumbledor";
+// const len = name.length;
+// const firstName = name.substring(29, 39);
+
+// console.log(firstName);
+
+const fullName = "Peter Heronimous Lind";
+
+const firstName = fullName.substring(0, 5);
+
+console.log(firstName);
+
+const middleName = fullName.substring(6, 16);
+
+console.log(middleName);
+
+const lastName = fullName.substring(17, 21);
+
+console.log(lastName);
