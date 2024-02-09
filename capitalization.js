be16d82baeb0ab9b06edf8373489fdc0c250dcbc
Chrys <chjo0001@stud.kea.dk>
2024-02-09 11:50:11 +0100

    capitalization

---
+++ capitalization.js
@@ -0,0 +1,19 @@
+const name = "Peter";
+
+const second = name.substring(0, 1);
+
+const upperSecond = second.toUpperCase();
+
+console.log(upperSecond);
+
+console.log(name);
+
+const first = name.substring(1, 5);
+
+const lowerFirst = first.toLowerCase();
+
+const joined = upperSecond + lowerFirst;
+
+console.log(joined)
+
+
